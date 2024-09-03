e4968fe436c50e31e2e44d9b7f08e6e35fe51d62
Casandra Brito Gomez <164533984+Casandra-Brito-0559@users.noreply.github.com>
2024-09-03 12:03:27 -0600

    Add files via upload

---
+++ milista.py
@@ -0,0 +1,26 @@
+# Ejemplo de uso de listas
+
+Misnovias=["Dahyun","Abril","Naeyon"]
+misnumeros=[666,77,10]
+
+# Mostrando mis nivias uwu
+print(Misnovias)
+
+# Mostrando mis numeros owo
+print(misnumeros)
+print("----Accediendo a los elementos de la lista---")
+print(Misnovias[0])
+
+if "xd" in Misnovias:
+    print("Si,'Dahyun' esta en la lista Mis Novias")
+else:
+    print("No,'Dahyun' no esta en la lista Mis Novias")
+
+Nnovias=len(Misnovias)
+print(f"Numero de novias = {Nnovias}")
+
+posicion=0
+
+for medianaranja in Misnovias:
+    print(posicion," ",medianaranja)
+    posicion=posicion+1
